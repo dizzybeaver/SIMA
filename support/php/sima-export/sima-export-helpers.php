b24beeeba72c33c1b0b560a1e92e1e89fe2adfe6
Joseph Hersey <jbhersey1977@gmail.com>
2025-11-27 07:03:07 -0500

    Create sima-export-helpers.php

---
+++ support/php/sima-export/sima-export-helpers.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * sima-export-helpers.php
+ * 
+ * Wrapper for Export Helper Functions
+ * Version: 2.0.0
+ * Date: 2025-11-27
+ * 
+ * REFACTORED: Split into modular files
+ */
+
+// Load modular components
+require_once __DIR__ . '/sima-export-archive.php';
+require_once __DIR__ . '/sima-export-manifest.php';
+require_once __DIR__ . '/sima-export-instructions.php';
+
+/**
+ * Create complete export archive (main entry point)
+ */
+function createExportArchive($validatedBase, $archiveName, $description, $selectedPaths, $sourceVersion, $targetVersion) {
+    return _createExportArchive($validatedBase, $archiveName, $description, $selectedPaths, $sourceVersion, $targetVersion);
+}
+?>
